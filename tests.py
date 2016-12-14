1f4057cf3b202ad48a4ef5815c9cf8071fa87512
WilsonYangLiu <lweixin316@gmail.com>
2016-12-14 15:45:58 +0800

    Change the example name and add clinical example

--- tests.py
+++ tests.py
@@ -10,27 +10,29 @@ from TCGADownloader.gdcFileClient import *
 os.chdir(r'E:/Project_G/db.TCGA/TCGADownloader')
 #os.chdir(r'/media/wilson/b776f228-366c-4e52-acd6-65df5b458e8c/Project_G/db.TCGA/TCGADownloader')
 
-def example_1():
-	print('Example 1:')
+def expression():
+	#print('Example 1:')
 	Downloader = gdcFileClient(r'./data/tmp_manifest.trans_GeneExp.tsv', DIR=r'./data',\
 								uuid_info_file=r'uuid_info_file.trans_GeneExp.tsv')
 	#Downloader.fileInfo()
 	Downloader.fileDownload()
 	
-def example_2():
-	print('Example 2:')
+def SNV():
+	#print('Example 2:')
 	Downloader = gdcSNVClient(r'./data/tmp_manifest.snv_mSomatic.tsv', DIR=r'./data',\
 								uuid_info_file=r'uuid_info_file.snv_mSomatic.json')
 	#Downloader.fileInfo()
 	Downloader.fileDownload()
 
-def example3():
-	Downloader = gdcSNVClient('./manifest.txt')
+def clinical():
+	Downloader = gdcFileClient('./data/tmp_manifest.clinical.tsv', DIR=r'./data',\
+								uuid_info_file=r'uuid_info_file.clinical.tsv')
 	Downloader._fields_retieved_str = ['file_id', 'cases_0_submitter_id', 'file_name']
 	Downloader.set_fields_retieved()
 	Downloader.fileDownload()
 
 if __name__ == '__main__':
-	#example_1()
-	#example_2()
-	example3()
+	#expression()
+	#SNV()
+	#clinical()
+	pass

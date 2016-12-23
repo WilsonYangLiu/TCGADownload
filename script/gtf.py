9eeafe4b074f8fa908430d12cc230648b6c96425
WilsonYangLiu <lweixin316@gmail.com>
2016-12-23 13:50:43 +0800

    Add option "To file" in script/gtf

--- script/gtf.py
+++ script/gtf.py
@@ -31,61 +31,97 @@ from itertools import dropwhile
 from pandas import DataFrame, Series
 from time import sleep
 
-def gtfClient(filename, selected_fld=None, attr_fld=None, LEN=None):
+def gtfClient(filename, selected_fld=None, attr_fld=None, LEN=None, To_file=False):
 	'''
 	Parameters:
 		filename: the gtf file, either ends with `gtf.gz` or `gtf`
 		selected_fld: int LIST. the fileds indexes (0-based, 0~8) that wanted to extract. There 9 fileds + 1 [comments]:
 			<seqname> <source> <feature> <start> <end> <score> <strand> <frame> [attributes] [comments]
 		attr_fld: STRING LIST. the fileds in [attributes] that want to extract. It can be the <gene_id>, <transcript_id>, <gene_name>, ...
+		LEN: the number of items in the file named `filename`
+		To_file: whether save the result to file derectly
 		
 	Returns: 
-		Data: DataFrame. 
+		Data: int 0 or DataFrame. 
 	'''
 	header = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attributes']
 	if filename.endswith('gtf.gz'):
 		gtfopen = gzip.open
+		outfile = '.'.join(filename.split('.')[:-2])
 	elif filename.endswith('gtf'):
 		gtfopen = open
+		outfile = '.'.join(filename.split('.')[:-1])
 	else:
 		raise IOError, r'the format of the file are not gtf!'
 		
 	with gtfopen(filename, 'rb') as handle:
-		Data = DataFrame()
 		idx = 0
 		if LEN < 10000:
 			LEN = None
-		for line in dropwhile(lambda line: line.startswith('#'), handle):
-			if LEN and (idx % 10000 == 0):
-				progbar(LEN, idx)
+		if To_file:
+			OUT = gzip.open('{}.csv.gz'.format(outfile), 'wb')
+			observe = ','.join([header[selected_idx] for selected_idx in selected_fld] + attr_fld)
+			OUT.write(observe)
+			OUT.write('\n')
 			
-			line = line.strip().split('\t')
-			attr = [itm.strip().split(' ') for itm in line[8].split(';')]
-			attr = {itm[0].strip():eval(itm[1]) for itm in attr if len(itm) > 1}
-			
-			fileds = {}
-			for i in selected_fld:
-				fileds[header[i] ] = line[i]
-			for j in attr_fld:
-				try:
-					fileds[j] = attr[j]
-				except Exception, e:
-					print(e)
-					fileds[j] = ''
-			
-			oneItem = Series(fileds)
-			if idx == 0:
-				oneItem.name = str(idx)
-				Data = DataFrame(oneItem)
-			else:
-				Data[str(idx)] = oneItem
+			for line in dropwhile(lambda line: line.startswith('#'), handle):
+				if LEN and (idx % 10000 == 0):
+					progbar(LEN, idx)
 				
-			idx = idx + 1
+				line = line.strip().split('\t')
+				attr = [itm.strip().split(' ') for itm in line[8].split(';')]
+				attr = {itm[0].strip():eval(itm[1]) for itm in attr if len(itm) > 1}
+				
+				attr_str = []
+				for j in attr_fld:
+					try:
+						attr_str.append(attr[j] )
+					except Exception, e:
+						print(e)
+						attr_str.append('.')
+
+				observe = ','.join([line[selected_idx] for selected_idx in selected_fld] + attr_str)
+				OUT.write(observe)
+				OUT.write('\n')
+				
+				idx = idx + 1
+				
+			print('\n')	
+			OUT.close()	
+			return 0
+				
+		else:
+			Data = DataFrame()
+			for line in dropwhile(lambda line: line.startswith('#'), handle):
+				if LEN and (idx % 10000 == 0):
+					progbar(LEN, idx)
+				
+				line = line.strip().split('\t')
+				attr = [itm.strip().split(' ') for itm in line[8].split(';')]
+				attr = {itm[0].strip():eval(itm[1]) for itm in attr if len(itm) > 1}
 			
-		print('\n')
-		Data = Data.T
-		
-	return Data
+				fileds = {}
+				for selected_idx in selected_fld:
+					fileds[header[selected_idx] ] = line[selected_idx]
+				for j in attr_fld:
+					try:
+						fileds[j] = attr[j]
+					except Exception, e:
+						#print(e)
+						fileds[j] = '.'
+				
+				oneItem = Series(fileds)
+				if idx == 0:
+					oneItem.name = str(idx)
+					Data = DataFrame(oneItem)
+				else:
+					Data[str(idx)] = oneItem
+							
+				idx = idx + 1
+			
+			print('\n')	
+			Data = Data.T
+			return Data
 	
 def progbar(LEN, i):
 	Interval = LEN // 60
@@ -95,21 +131,22 @@ def progbar(LEN, i):
 		sys.stdout.flush()
 		
 def work():
-	os.chdir(r'E:/Project_G/db.TCGA/TCGADownloader/script')
-	data = gtfClient(r'../data/gencode.v22.annotation.gtf.gz', selected_fld=[3, 4], attr_fld=['gene_id', 'gene_name'], LEN=2563676)
-	data['length'] = pd.to_numeric(data['end']) - pd.to_numeric(data['start'])
-	del data['end']
-	del data['start']
-	data.to_csv(r'./gencode.v22.annotation.filter.csv')
+	data = gtfClient(r'./gencode.v22.annotation.gtf.gz', selected_fld=[3, 4], attr_fld=['gene_id', 'gene_name'], LEN=2563676, To_file=True)
+	if isinstance(data, DataFrame):
+		data['length'] = pd.to_numeric(data['end']) - pd.to_numeric(data['start'])
+		del data['end']
+		del data['start']
+		data.to_csv(r'./gencode.v22.annotation.filter.csv')
 			
 if __name__ == '__main__':
 	#work()
 	
 	os.chdir(r'E:/Project_G/db.TCGA/TCGADownloader/script')
-	data = gtfClient(r'../data/tmp.gencode.annotation.gtf', selected_fld=[3,4], attr_fld=['gene_id'], LEN=336)
-	data['length'] = pd.to_numeric(data['end']) - pd.to_numeric(data['start'])
-	del data['end']
-	del data['start']
-	data.to_csv(r'../data/tmp.gencode.annotation.filter.csv')
+	data = gtfClient(r'../data/tmp.gencode.annotation.gtf.gz', selected_fld=[3,4], attr_fld=['gene_id'], LEN=336, To_file=False)
+	if isinstance(data, DataFrame):
+		data['length'] = pd.to_numeric(data['end']) - pd.to_numeric(data['start'])
+		del data['end']
+		del data['start']
+		data.to_csv(r'../data/tmp.gencode.annotation.filter.csv')
 
 	
